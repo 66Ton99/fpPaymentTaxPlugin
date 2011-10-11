c0da5010e2e978bbd316f77cd9039ea557e65be3
Ton Sharp <Forma-PRO@66ton99.org.ua>
2011-10-11 17:41:46 +0300

    Changed schema

---
+++ lib/enum/fpPaymentTaxDataZipTypeEnum.class.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Tax zip_type enum
+ *
+ * @package    fpPayment
+ * @subpackage Base
+ * @author     Ton Sharp <Forma-PRO@66ton99.org.ua>
+ */
+class fpPaymentTaxDataZipTypeEnum extends fpPaymentEnum
+{
+
+  const NONE = 'none';
+  const SINGLE = 'single';
+  const MULTIPLE = 'multiple';
+  const RANGE = 'range';
+  
+}
